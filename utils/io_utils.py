67be1f36954ff6c567a9bc9bd330b3653b6be359
David Mummy <126700227+davidmummy@users.noreply.github.com>
2024-11-21 14:56:58 -0500

    73 error reading mrd file 1 (#83)
    * Addressed issue 73
    
    * adding .mrd read-in for proton and cal files
    
    ---------
    
    Co-authored-by: YonniFriedlander <yonni@LAPTOP-NIP2N6OH.localdomain>
    Co-authored-by: dgm19 <dgm19@duke.edu>

--- utils/io_utils.py
+++ utils/io_utils.py
@@ -146,7 +146,9 @@ def get_dyn_mrd_files(path: str) -> str:
     try:
         return (
             glob.glob(os.path.join(path, "**Calibration***.h5"))
-            + glob.glob(os.path.join(path, "**calibration***.h5"))
+            + glob.glob(os.path.join(path, "**calibration***.h5")) 
+            + glob.glob(os.path.join(path, "**Calibration***.mrd"))
+            + glob.glob(os.path.join(path, "**calibration***.mrd"))
         )[0]
     except:
         raise ValueError("Can't find MRD file in path.")
@@ -161,7 +163,10 @@ def get_dis_mrd_files(path: str) -> str:
         str file path of MRD file
     """
     try:
-        return (glob.glob(os.path.join(path, "**dixon***.h5")))[0]
+        return (
+            glob.glob(os.path.join(path, "**dixon***.h5"))
+            + glob.glob(os.path.join(path, "**dixon***.mrd"))
+        )[0]
     except:
         raise ValueError("Can't find MRD file in path.")
 
@@ -175,7 +180,10 @@ def get_ute_mrd_files(path: str) -> str:
         str file path of MRD file
     """
     try:
-        return (glob.glob(os.path.join(path, "**proton***.h5")))[0]
+        return (
+            glob.glob(os.path.join(path, "**proton***.h5"))
+            + glob.glob(os.path.join(path, "**proton***.mrd"))
+        )[0]
     except:
         raise ValueError("Can't find MRD file in path.")
 
