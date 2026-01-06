7c24f27da52af32cb4fb09d2165db3a60d6c5aff
kimi-du <kimi.du2017@gmail.com>
2026-01-06 15:01:40 -0500

    Stop tracking local histogram helper scripts

--- visualize_hist.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-hp = np.load("data/FV_healthy_ref/12-11-25_reference_hist_profile.npy")
-print(hp.shape)           # (2, 50)
-centers = hp[0, :]        # bin centers
-probs   = hp[1, :]        # probabilities
